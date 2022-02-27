a22ad9d6bbb705af681da0b53909e78914d95275
enoc pineda <enocpinedaa@gmail.com>
2022-02-26 22:16:10 -0600

    Routing Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
 import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRouterModule,
     BrowserModule,
     PrimeNgModule,
     SharedModule
