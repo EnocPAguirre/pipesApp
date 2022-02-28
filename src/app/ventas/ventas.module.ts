82360f00fe2618c28462e2cd1db24a937ba25ae5
enoc pineda <enocpinedaa@gmail.com>
2022-02-27 21:18:58 -0600

    PrimeFlex

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -4,13 +4,15 @@ import { NumerosComponent } from './pages/numeros/numeros.component';
 import { NoComunesComponent } from './pages/no-comunes/no-comunes.component';
 import { BasicosComponent } from './pages/basicos/basicos.component';
 import { OrdenarComponent } from './pages/ordenar/ordenar.component';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
 
 
 @NgModule({
   declarations: [NumerosComponent, NoComunesComponent, BasicosComponent, OrdenarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    PrimeNgModule
   ],
   exports: [NumerosComponent, NoComunesComponent, BasicosComponent, OrdenarComponent]
 })
